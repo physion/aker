9304f71c6e84a19a8f80dedd19d334f69d0da4dd
Barry Wark <barrywark@gmail.com>
2014-10-06 00:15:42 -0400

    whoops

--- application.py
+++ application.py
@@ -33,7 +33,8 @@ if 'AKER_CONFIG_PATH' in os.environ:
 config_overrides = ['COUCH_HOST', 'COUCH_USER', 'COUCH_PASSWORD',
                     'DB_UPDATES_SQS_QUEUE', 'SECRET_KEY', 'UNDERWORLD_DATABASE']
 
-logging.debug("Override app config: " + str(app.config))
+
+logging.debu("OS ENVIRON {}".format(str(os.environ)))
 
 for k in config_overrides:
     if k in os.environ:
